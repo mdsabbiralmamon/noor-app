0547bf1a641c99fbb8c5d14f6a9e32ac6522af94
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-09-19 02:07:02 +0600

    Refactor layout component and add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from "@/components/home/Hero/Hero";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <h1>Coming Soon ...</h1>
+    <div className="">
+      <Hero />
     </div>
   );
 }
