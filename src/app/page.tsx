45c650ccb52e06056e2373cf3d1e98ae5463899b
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-09-19 20:34:04 +0600

    Add About and PrayerTimes components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import About from "@/components/home/About/About";
 import Hero from "@/components/home/Hero/Hero";
+import PrayerTimes from "@/components/home/PrayerTimes/PrayerTimes";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <About />
+      <PrayerTimes />
     </div>
   );
 }
