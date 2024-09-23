c0e4f399b84106ae95ab7c6140ec3bdac47db9be
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-09-23 14:57:51 +0600

    Add MosqueDev and School components to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import About from "@/components/home/About/About";
 import Hero from "@/components/home/Hero/Hero";
+import MosqueDev from "@/components/home/MosqueDev/MosqueDev";
 import PrayerTimes from "@/components/home/PrayerTimes/PrayerTimes";
+import School from "@/components/home/School/School";
+import './page.css';
 
 export default function Home() {
   return (
@@ -8,6 +11,10 @@ export default function Home() {
       <Hero />
       <About />
       <PrayerTimes />
+      <div className="curveSectionBG">
+        <School />
+        <MosqueDev />
+      </div>
     </div>
   );
 }
