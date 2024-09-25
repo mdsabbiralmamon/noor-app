1670ec2f6154de5a7627380cc15fdde1be9eaa17
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-09-25 15:43:47 +0600

    Add FivePillars, Events, Support, Blog, and Newsletter components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import MosqueDev from "@/components/home/MosqueDev/MosqueDev";
 import PrayerTimes from "@/components/home/PrayerTimes/PrayerTimes";
 import School from "@/components/home/School/School";
 import './page.css';
+import FivePillars from "@/components/home/FivePillars/FivePillars";
+import Events from "@/components/home/Events/Events";
+import Support from "@/components/home/Support/Support";
+import Blog from "@/components/home/Blog/Blog";
+import Newsletter from "@/components/ui/newsletter";
 
 export default function Home() {
   return (
@@ -14,6 +19,18 @@ export default function Home() {
       <div className="curveSectionBG">
         <School />
         <MosqueDev />
+      </div>
+      <FivePillars />
+      <Events />
+      <Support />
+      <Blog />
+      <div className="relative bg-gray-50 py-20">
+        <div className="absolute inset-0 bg-white h-1/2" />
+        <div className="relative z-10 max-w-4xl mx-auto">
+          <div>
+            <Newsletter />
+          </div>
+        </div>
       </div>
     </div>
   );
