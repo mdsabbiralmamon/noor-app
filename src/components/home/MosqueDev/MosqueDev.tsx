544cebc917ed19b345cf6f79a9c65e1346a84e7f
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-09-25 15:44:18 +0600

    Add Counter component to MosqueDev

--- src/components/home/MosqueDev/MosqueDev.tsx
+++ src/components/home/MosqueDev/MosqueDev.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import React from 'react';
 import mosqueImg from '../../../../public/images/mosque.png';
+import Counter from '@/components/ui/counter';
 
 const MosqueDev = () => {
     return (
@@ -23,7 +24,11 @@ const MosqueDev = () => {
                     <p className="text-lg text-gray-600">
                         The Weekend School of the Islamic Center of Allah is committed to providing quality Islamic Education according to the Quran and the Sunnah of the Prophet Mohammed (Peace Be upon Him) to a diverse student population in the greater area.
                     </p>
-                    
+                    <div className='flex justify-evenly flex-col md:flex-row'>
+                        <Counter countTo={45} label="New Mosque" />
+                        <Counter countTo={35} label="Old Mosque" />
+                        <Counter countTo={90} label="Imams" />
+                    </div>
                 </div>
             </div>
         </div>
