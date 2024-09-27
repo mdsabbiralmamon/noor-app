6e112c5020e310cad46995357b6a7b35bfcf8087
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-09-27 15:45:00 +0600

    Refactor School component to update styling and add margin-bottom to text elements

--- src/components/home/School/School.tsx
+++ src/components/home/School/School.tsx
@@ -9,17 +9,17 @@ const School = () => {
             <div className='container px-8 py-24 mx-auto h-[1080px] flex flex-col-reverse md:flex-row items-center justify-between gap-12'>
                 {/* Text Section */}
                 <div className="flex-1">
-                    <span className="inline-block bg-white text-primary font-semibold py-2 px-4 rounded-full text-sm">
+                    <span className="inline-block bg-white text-primary font-semibold py-2 px-4 rounded-full text-sm mb-4 shadow-md bg-primary-foreground">
                         Islamic School
                     </span>
-                    <h1 className="text-4xl md:text-6xl font-bold text-primary leading-tight">
+                    <h1 className="text-4xl md:text-6xl font-bold text-primary leading-tight mb-4">
                         Seek knowledge from the cradle to the grave.
                     </h1>
-                    <p className="text-lg text-gray-600">
+                    <p className="text-lg text-gray-600 mb-4">
                         The Weekend School of the Islamic Center of Allah is committed to providing quality Islamic Education according to the Quran and the Sunnah of the Prophet Mohammed (Peace Be upon Him) to a diverse student population in the greater area.
                     </p>
                     {/* Academic Table Data */}
-                    <table className="table-auto border-separate" style={{ borderSpacing: '0 10px' }}>
+                    <table className="table-auto border-separate mb-4" style={{ borderSpacing: '0 10px' }}>
                         <thead>
                             <tr className='bg-gradient-to-r from-[#f6d365] to-[#fda085] shadow-lg'>
                                 <th className="md:px-4 py-2 rounded-l-lg">School Calendar</th>
