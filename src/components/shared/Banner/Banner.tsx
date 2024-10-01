a7b43d0e0d772f62d01ddab9612b10d5310a2fb9
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-10-01 22:20:49 +0600

    Refactor Banner component to update background color

--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -6,7 +6,7 @@ type BannerProps = {
 
 const Banner: React.FC<BannerProps> = ({ title }) => {
     return (
-        <div className='bg-primary-foreground h-96 w-full flex justify-center items-center'>
+        <div className='bg-gray-50 h-96 w-full flex justify-center items-center'>
             <div className='mt-20 text-center space-y-8'>
                 <h3 className='text-primary text-4xl font-semibold'>{title}</h3>
                 <h4 className='text-xl text-primary'><a href='/' className='text-black'>home</a>/{title}</h4>
