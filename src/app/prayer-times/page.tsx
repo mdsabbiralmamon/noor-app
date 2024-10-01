6a222cf73a0e4d442490d68d86ed8d132cc5a19c
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-10-01 22:20:38 +0600

    Add HeroBanner component to PrayerTimes page

--- src/app/prayer-times/page.tsx
+++ src/app/prayer-times/page.tsx
@@ -1,3 +1,4 @@
+import HeroBanner from '@/components/prayerTimes/HeroBanner/HeroBanner';
 import Banner from '@/components/shared/Banner/Banner';
 import React from 'react';
 
@@ -5,6 +6,9 @@ const PrayerTimes = () => {
     return (
         <div>
             <Banner title={'Prayer Times'} />
+            <div className='container mx-auto'>
+                <HeroBanner />
+            </div>
         </div>
     );
 };
